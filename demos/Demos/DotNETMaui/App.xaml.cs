b5da4d9b6c4bb0ac67d68d7ef558c4d8e1c8b6f9
Jimmy Engström <jimmy@apeoholic.se>
2022-04-21 11:37:39 +0200

    Added .NET MAUI demo

---
+++ demos/Demos/DotNETMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNETMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ demos/Demos/DotNETMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNETMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
